1a41366f84040d516ce5bfd9c5adb5ba78f6db08
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-11 15:17:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,3 @@
-# don't forget to add: require 'pry'
 require 'pry'
 
 def get_user_input
@@ -13,7 +12,7 @@ end
 
 def selection(num)
   if num == 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+    "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
     "HAM HAM HAM IN MY TUMMY"
   end
